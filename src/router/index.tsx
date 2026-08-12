f2f7488dc44682435756f0ecc1af7000ba0f2738
fengmengyou <2022108200@qq.com>
2026-08-12 14:50:41 +0800

    feat: 增加账号提示时间

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -2,17 +2,27 @@ import { lazy } from "react";
 import { createBrowserRouter, createHashRouter, Navigate } from "react-router";
 import App from "@src/App";
 import AuthGuard from "@src/components/AuthGuard";
+import Dashboard from "@src/pages/Dashboard";
+import Attendance from "@src/pages/Attendance";
+import Staff from "@src/pages/Staff";
+import Department from "@src/pages/Department";
+import Level from "@src/pages/Level";
+import Assessment from "@src/pages/Assessment";
+import Salary from "@src/pages/Salary";
+import RewardRecord from "@src/pages/RewardRecord";
+import Login from "@src/pages/Login";
+import NotFound from "@src/pages/NotFound";
 
-const Dashboard = lazy(() => import("@src/pages/Dashboard"));
-const Attendance = lazy(() => import("@src/pages/Attendance"));
-const Staff = lazy(() => import("@src/pages/Staff"));
-const Department = lazy(() => import("@src/pages/Department"));
-const Level = lazy(() => import("@src/pages/Level"));
-const Assessment = lazy(() => import("@src/pages/Assessment"));
-const Salary = lazy(() => import("@src/pages/Salary"));
-const RewardRecord = lazy(() => import("@src/pages/RewardRecord"));
-const Login = lazy(() => import("@src/pages/Login"));
-const NotFound = lazy(() => import("@src/pages/NotFound"));
+// const Dashboard = lazy(() => import("@src/pages/Dashboard"));
+// const Attendance = lazy(() => import("@src/pages/Attendance"));
+// const Staff = lazy(() => import("@src/pages/Staff"));
+// const Department = lazy(() => import("@src/pages/Department"));
+// const Level = lazy(() => import("@src/pages/Level"));
+// const Assessment = lazy(() => import("@src/pages/Assessment"));
+// const Salary = lazy(() => import("@src/pages/Salary"));
+// const RewardRecord = lazy(() => import("@src/pages/RewardRecord"));
+// const Login = lazy(() => import("@src/pages/Login"));
+// const NotFound = lazy(() => import("@src/pages/NotFound"));
 
 export const routeList: any = [
   {
